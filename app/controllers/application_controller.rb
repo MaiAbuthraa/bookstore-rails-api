4ed16bc3588170c382dd3d8b15030f54a4d830b9
MaiAbuthraa <mai.abuthraa@gmail.com>
2020-11-13 13:46:01 +0200

    Use rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  rescue_from NoMethodError, with: :no_method
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_record
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+  def not_destroyed(e)
+    render json: { errors: e.record.errors }, status: :unprocessable_entity
+  end
+
+  def no_method(e)
+    puts ""
+    return render json: { errors: e.record.errors }, status: :unprocessable_entity 
+  end
+
+  def not_found_record(e)
+    return render json: { errors: e.record.errors }, status: :not_found
+  end
 end
